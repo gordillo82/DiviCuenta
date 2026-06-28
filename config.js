cabc6cf1b2228c0558a9c06c7f632c9275ecfc1a
gordillo82 <gordillojorquesangel@gmail.com>
2026-06-28 14:25:01 +0200

    Update SUPABASE_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://sgxmgqlqwjxmgwwggusm.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://sgxmgqlqwjxmgwwggusm.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_0ehaq1eUhnTNX5VDL_bMMg_hVt5yxUh",
 };
